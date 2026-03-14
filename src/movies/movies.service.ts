187ba97e221ec4ed15ac739008a22dbb70034945
dj-pmfst <djuric@pmfst.hr>
2026-03-14 17:21:22 +0100

    create movie service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll(search?: string, sort?: string, genre?: string) {
+    return this.prisma.movie.findMany({
+      where: {
+        title: search ? { contains: search, mode: 'insensitive' } : undefined,
+        genres: genre ? { some: { name: genre } } : undefined
+      },
+      orderBy: sort ? { [sort]: 'asc' } : undefined,
+      include: { genres: true }
+    })
+  }
+
+  findOne(id: number) {
+    return this.prisma.movie.findUnique({ 
+      where: { id },
+      include: { genres: true }
+    })
+  }
+
+  create(dto: CreateMovieDto) {
+    return this.prisma.movie.create({ data: dto })
+  }
+
+  update(id: number, dto: UpdateMovieDto) {
+    return this.prisma.movie.update({ where: { id }, data: dto })
+  }
+
+  remove(id: number) {
+    return this.prisma.movie.delete({ where: { id } })
+  }
+}
