455539bf378a68fd2ca8238884bc8f912e577f81
dj-pmfst <djuric@pmfst.hr>
2026-03-14 22:33:15 +0100

    fetch poster images

--- prisma/movies.ts
+++ prisma/movies.ts
@@ -6,7 +6,7 @@ export const movies = [
         rating: 9.0,
         genre: "Action",
         description: "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.",
-        poster: '/src/assets/The_Dark_Knight.jpg'
+        poster: 'http://localhost:3000/posters/The_Dark_Knight.jpg'
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const movies = [
         rating: 8.8,
         genre: "Sci-fi",
         description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO, but his tragic past may doom the project and his team to disaster.",
-        poster: "/src/assets/Inception.jpg"
+        poster: "http://localhost:3000/posters/Inception.jpg"
     },
     {
         id: 3,
@@ -24,7 +24,7 @@ export const movies = [
         rating: 9.2,
         genre: "Drama",
         description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-        poster: "/src/assets/godfather.jpg"
+        poster: "http://localhost:3000/posters/godfather.jpg"
     },
     {
         id: 4,
@@ -33,7 +33,7 @@ export const movies = [
         rating: 8.9,
         genre:"Fantasy",
         description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
-        poster: "/src/assets/Lord_Rings_Fellowship_Ring.jpg"
+        poster: "http://localhost:3000/posters/Lord_Rings_Fellowship_Ring.jpg"
     },
     {
         id: 5,
@@ -42,7 +42,7 @@ export const movies = [
         rating: 8.7,
         genre: "Sci-fi",
         description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans." ,
-        poster: "/src/assets/Interstellar.jpg"
+        poster: "http://localhost:3000/posters/Interstellar.jpg"
     },
     {
         id: 6,
@@ -51,7 +51,7 @@ export const movies = [
         rating: 7.8,
         genre: "Action",
         description: "In a time of conflict, a group of unlikely heroes band together on a mission to steal the plans to the Death Star, the Empire's ultimate weapon of destruction.",
-        poster: "/src/assets/Rogue_One.png"
+        poster: "http://localhost:3000/posters/Rogue_One.png"
     },
     {
         id: 7,
@@ -60,7 +60,7 @@ export const movies = [
         rating: 8.5,
         genre: "Drama",
         description: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
-        poster: "/src/assets/Parasite.png"
+        poster: "http://localhost:3000/posters/Parasite.png"
     },
     {
         id: 8,
@@ -69,7 +69,7 @@ export const movies = [
         rating: 6.7,
         genre: "Sci-fi",
         description: "During a human expedition to colonize space, Mickey 17, a so-called expendable employee, is sent to explore an ice planet.",
-        poster: "/src/assets/Mickey_17.png"
+        poster: "http://localhost:3000/posters/Mickey_17.png"
     },
     {
         id: 9,
@@ -78,7 +78,7 @@ export const movies = [
         rating: 6.8,
         genre: "Fantasy",
         description: "Barbie and Ken are having the time of their lives in the seemingly perfect world of Barbie Land. However, when they get a chance to go to the outside world, they soon discover the joys and perils of living among regular humans.",
-        poster: "/src/assets/Barbie.jpg"
+        poster: "http://localhost:3000/posters/Barbie.jpg"
     },
     {
         id: 10,
@@ -87,7 +87,7 @@ export const movies = [
         rating: 7.9,
         genre: "Fantasy",
         description: "Cat is a solitary animal, but as its home is devastated by a great flood, he finds refuge on a boat populated by various species, and will have to team up with them despite their differences.",
-        poster: "/src/assets/Flow.jpg"
+        poster: "http://localhost:3000/posters/Flow.jpg"
     },
     {
         id: 11,
@@ -96,7 +96,7 @@ export const movies = [
         rating: 8.5,
         genre: "Drama",
         description: "A young boy and his little sister struggle to survive in Japan during World War II.",
-        poster: "/src/assets/Grave_of_the_Fireflies.jpg"
+        poster: "http://localhost:3000/posters/Grave_of_the_Fireflies.jpg"
     },
     {
         id: 12,
@@ -105,7 +105,7 @@ export const movies = [
         rating: 8.2,
         genre: "Action",
         description: "April 6th, 1917. As an infantry battalion assembles to wage war deep in enemy territory, two soldiers are assigned to race against time and deliver a message that will stop 1,600 men from walking straight into a deadly trap.",
-        poster: "/src/assets/1917.jpg"
+        poster: "http://localhost:3000/posters/1917.jpg"
     },
     {
         id: 13,
@@ -114,7 +114,7 @@ export const movies = [
         rating: 8.8,
         genre: "Drama",
         description: "The history of the United States from the 1950s to the '70s unfolds from the perspective of an Alabama man with an IQ of 75, who yearns to be reunited with his childhood sweetheart.",
-        poster: "/src/assets/Forrest_Gump.jpg"
+        poster: "http://localhost:3000/posters/Forrest_Gump.jpg"
     },
     {
         id: 14,
@@ -123,7 +123,7 @@ export const movies = [
         rating: 8.2,
         genre: "Action",
         description: "An industrialist invites some experts to visit his theme park of cloned dinosaurs. After a power failure, the creatures run loose, putting everyone's lives, including his grandchildren's, in danger.",
-        poster: "/src/assets/Jurassic_Park.jpg"
+        poster: "http://localhost:3000/posters/Jurassic_Park.jpg"
     },
     {
         id: 15,
@@ -132,7 +132,7 @@ export const movies = [
         genre: "Fantasy",
         rating: 8.1,
         description: "A hapless young Viking who aspires to hunt dragons becomes the unlikely friend of a young dragon himself, and learns there may be more to the creatures than he assumed.",
-        poster: "/src/assets/How_to_Train_Your_Dragon.jpg"
+        poster: "http://localhost:3000/posters/How_to_Train_Your_Dragon.jpg"
     },
     {
         id: 16,
@@ -141,7 +141,7 @@ export const movies = [
         genre: "Action",
         rating: 7.2,
         description: "Katniss Everdeen voluntarily takes her younger sister's place in the Hunger Games: a televised competition in which two teenagers from each of the twelve Districts of Panem are chosen at random to fight to the death.",
-        poster: "/src/assets/HungerGames.jpg"
+        poster: "http://localhost:3000/posters/HungerGames.jpg"
     },
     {
         id: 17,
@@ -150,7 +150,7 @@ export const movies = [
         genre: "Drama",
         rating: 8.3,
         description: "Despite being caught in her imaginative world, young waitress Amelie decides to help people find happiness. Her quest to spread joy leads her on a journey during which she finds true love.",
-        poster: "/src/assets/Amelie.jpg"
+        poster: "http://localhost:3000/posters/Amelie.jpg"
     },
     {
         id: 18,
@@ -159,7 +159,7 @@ export const movies = [
         genre: "Drama",
         description: "Independent hotelier Donna is preparing for her daughter's wedding with the help of two old friends. Meanwhile, spirited bride-to-be Sophie invites three men from her mother's past in order to discern which is her biological father.",
         rating: 6.5,
-        poster: "/src/assets/Mamma_Mia.jpg"
+        poster: "http://localhost:3000/posters/Mamma_Mia.jpg"
     },
     {
         id: 19,
@@ -168,7 +168,7 @@ export const movies = [
         genre: "Sci-fi",
         description: "When a mysterious artifact is uncovered on the Moon, a spacecraft manned by two humans and one supercomputer is sent to Jupiter to find its origins.",
         rating: 8.3,
-        poster: "/src/assets/2001.png"
+        poster: "http://localhost:3000/posters/2001.png"
     },
     {
         id: 20, 
@@ -177,7 +177,7 @@ export const movies = [
         rating: 7.3,
         genre: "Action",
         description: "Armed with only the word Tenet, and fighting for the survival of the entire world, CIA operative, The Protagonist, journeys through a twilight world of international espionage on a global mission that unfolds beyond real time.",
-        poster: "/src/assets/Tenet.jpg"
+        poster: "http://localhost:3000/posters/Tenet.jpg"
     },
     {
         id: 21,
@@ -186,7 +186,7 @@ export const movies = [
         rating: 7.9,
         genre: "Sci-fi",
         description: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
-        poster: "/src/assets/Iron_Man.jpg"
+        poster: "http://localhost:3000/posters/Iron_Man.jpg"
     },
     {
         id: 22,
@@ -195,7 +195,7 @@ export const movies = [
         rating: 7.9,
         genre: "Sci-fi",
         description: "In a future Earth in which aliens are isolated in a remote ghetto, a government agent finds himself banished there.",
-        poster: "/src/assets/District.jpg"
+        poster: "http://localhost:3000/posters/District.jpg"
     },
     {
         id: 23,
@@ -204,7 +204,7 @@ export const movies = [
         rating: 8.2,
         genre: "Fantasy",
         description: "When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.",
-        poster: "/src/assets/Howls.jpg"
+        poster: "http://localhost:3000/posters/Howls.jpg"
     },
     {
         id: 24,
@@ -213,6 +213,6 @@ export const movies = [
         rating: 7.4,
         genre: "Fantasy",
         description: "",
-        poster: "/src/assets/Corpse_Bride.jpg"
+        poster: "http://localhost:3000/posters/Corpse_Bride.jpg"
     }
 ]
