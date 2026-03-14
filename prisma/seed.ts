63551dce3512fe87e2cfbf595896e4b4cc43d0f3
dj-pmfst <djuric@pmfst.hr>
2026-03-14 01:44:47 +0100

    start prism implementation

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { PrismaClient } from '../src/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
