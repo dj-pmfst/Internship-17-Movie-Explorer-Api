0853451ef95b135b7157939dd7b4e4b4d2abfd7f
dj-pmfst <djuric@pmfst.hr>
2026-03-14 13:54:57 +0100

    create seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,51 @@
-import 'dotenv/config';
-import { PrismaClient } from '../src/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../src/generated/prisma/client'
+import { movies } from './movies' 
+import 'dotenv/config'
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient()
 
 async function main() {
+
+  const genres = await prisma.genre.createMany({
+    data: [
+      { name: 'Action' },
+      { name: 'Drama' },
+      { name: 'Sci-fi' },
+      { name: 'Fantasy' }
+    ],
+    skipDuplicates: true
+  })
+
+  const genreRecords = await prisma.genre.findMany()
+
+  const genreMap = Object.fromEntries(
+    genreRecords.map(g => [g.name.toLowerCase(), g.id])
+  )
+
+  for (const movie of movies) {
+    await prisma.movie.create({
+      data: {
+        title: movie.title,
+        year: movie.year,
+        rating: movie.rating ?? null,
+        description: movie.description ?? '',
+        poster: movie.poster,
+
+        genres: {
+          connect: [
+            { id: genreMap[movie.genre.toLowerCase()] }
+          ]
+        }
+      }
+    })
+  }
 }
 
 main()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
