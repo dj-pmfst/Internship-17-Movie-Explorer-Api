4a934f07085c075a7682cd9d725d8791f5a43bb2
dj-pmfst <djuric@pmfst.hr>
2026-03-14 18:23:22 +0100

    add favourites service

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { CreateFavouriteDto } from './dto/create-favourite.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+    constructor(private prisma: PrismaService) {}
+
+    create(dto: CreateFavouriteDto) {
+        return this.prisma.favourite.create({
+            data: { movieId: dto.movieId },
+            include: { movie: true }  
+        })
+    }
+
+    findAll() {
+        return this.prisma.favourite.findMany({
+            include: { movie: true }  
+        })
+    }
+
+    remove(id: number) {
+        return this.prisma.favourite.delete({
+            where: { id }
+        })
+    }
+}
