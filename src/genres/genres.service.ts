96ca7482f45d811530158769e7d135939a29ea72
dj-pmfst <djuric@pmfst.hr>
2026-03-14 18:48:22 +0100

    add genre service

--- src/genres/genres.service.ts
+++ src/genres/genres.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateGenreDto } from './dto/create-genre.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class GenresService {}
+export class GenresService {
+    constructor(private prisma: PrismaService) {}
+
+    create(dto: CreateGenreDto) {
+        return this.prisma.genre.create({
+            data: {name: dto.name},
+        })
+    }
+
+    findAll() {
+        return this.prisma.genre.findMany({
+            include: { movies: true }  
+        })
+    }
+}
