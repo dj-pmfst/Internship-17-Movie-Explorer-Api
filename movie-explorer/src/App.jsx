e092523dc27500040d736026b5ecc7303181d906
dj-pmfst <djuric@pmfst.hr>
2026-03-20 19:42:42 +0100

    add login/register page

--- movie-explorer/src/App.jsx
+++ movie-explorer/src/App.jsx
@@ -5,13 +5,15 @@ import Movies from "./pages/movies/movies"
 import MovieDetail from "./pages/details/details"
 import Favourites from "./pages/favourites/favourites"
 import NotFound from "./pages/error/error"
+import Auth from "./pages/auth/auth"
 
 function App() {
 
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Auth />}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/movies" element={<Movies />}></Route>
         <Route path="/movies/:id" element={<MovieDetail />} />
         <Route path="/favourites" element={<Favourites />}></Route>
